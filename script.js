970dc4df3945d344820e9858d2b97f95723f1c5e
Katherine De La Cruz <katherine.delacruz8@gmail.com>
2016-02-20 01:02:03 -0500

    Add floating button

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
   $('#username, #password').keydown(button_validate);
   $('.color').click( function () {
     $(this).addClass('click');
-  })
+  });
+  anonymus();
 });
 
 function button_validate() {
@@ -13,4 +14,31 @@ function button_validate() {
   else {
     $('.button').removeClass('wrapper');
   }
+};
+
+function anonymus () {
+ var tlMenu = new TimelineLite({
+   paused: true
+ });
+ el = $(".menu-block");
+ tlMenu.to(el, .8, {
+     margin: 0,
+     padding: 40,
+     width: '361px',
+     height: '455px',
+     borderRadius: 10,
+     ease: Back.easeInOut
+   })
+
+  $('.menu-block .fa-plus').on('click', function() {
+    if($(this).hasClass('.menu-active')){
+      tlMenu.reverse();
+      $(this).removeClass('.menu-active');
+      $('.menu-block').removeClass('menu-active');
+    } else {
+      tlMenu.play();
+      $(this).addClass('.menu-active');
+      $('.menu-block').addClass('menu-active');
+    }
+  });
 }
